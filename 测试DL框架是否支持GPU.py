0f47b60f4322c4de6bb976cc0a2c8cd1bf1775c0
TonyEinstein <ruhai.chen@foxmail.com>
2024-02-20 16:09:19 +0800

    更新依赖文件，以及测试是否还正常上传。

--- 测试DL框架是否支持GPU.py
+++ 测试DL框架是否支持GPU.py
@@ -32,14 +32,15 @@ try:
 
     a = tf.test.is_built_with_cuda()  # 判断CUDA是否可以用
     # b = tf.test.is_gpu_available(cuda_only=False,min_cuda_compute_capability=None)  # 判断GPU是否可以用
-    if tf.config.list_physical_devices('GPU'):
+    op = tf.config.list_physical_devices('GPU')
+    if op:
         b = True
     else:
         b = False
 
     print("tensorflow：CUDA是否可用：\t",a)
     print("tensorflow：GPU是否可用：\t",b)
-    print("tensorflow：GPU详细信息：：\t",tf.config.list_physical_devices('GPU'))
+    print("tensorflow：GPU详细信息：：\t",op)
 except Exception as e:
     print(e)
 
