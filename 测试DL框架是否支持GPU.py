0342be5c292ccadbeb41c2402629158faad703f2
TonyEinstein <ruhai.chen@foxmail.com>
2024-02-20 16:23:16 +0800

    更新依赖文件，以及测试是否还正常上传。

--- 测试DL框架是否支持GPU.py
+++ 测试DL框架是否支持GPU.py
@@ -10,6 +10,10 @@
 try:
     # 判断是否安装了cuda
     import torch
+    print("torch：版本：\t",torch.__version__)
+    print("CUDA：版本：\t",torch.version.cuda)
+    print("CUDNN：版本：\t",torch.backends.cudnn.version())
+
     print("torch：是否已安装CUDA：\t",torch.cuda.is_available())  # 返回True则说明已经安装了cuda
     # 判断是否安装了cuDNN
     from torch.backends import cudnn
@@ -27,18 +31,20 @@ except Exception as e:
 print("--------------"*5)
 try:
     import tensorflow as tf
+    print("tensorflow：版本：\t",tf.__version__)
+    if tf.test.is_built_with_cuda():
+        print("CUDA 版本:", tf.sysconfig.get_build_info()['cuda_version'])
+        print("CUDNN 版本:", tf.sysconfig.get_build_info()['cudnn_version'])
 
-    print("tensorflow：可用的GPU数量: \t", len(tf.config.experimental.list_physical_devices('GPU')))
-
-    a = tf.test.is_built_with_cuda()  # 判断CUDA是否可以用
     # b = tf.test.is_gpu_available(cuda_only=False,min_cuda_compute_capability=None)  # 判断GPU是否可以用
     op = tf.config.list_physical_devices('GPU')
     if op:
         b = True
     else:
         b = False
-
-    print("tensorflow：CUDA是否可用：\t",a)
+    print("tensorflow：可用的GPU数量: \t", len(tf.config.experimental.list_physical_devices('GPU')))
+    print("tensorflow：CUDA是否可用：\t",tf.test.is_built_with_cuda())
+    print("tensorflow：CUDNN是否可用：\t",tf.test.is_built_with_cudnn())
     print("tensorflow：GPU是否可用：\t",b)
     print("tensorflow：GPU详细信息：：\t",op)
 except Exception as e:
